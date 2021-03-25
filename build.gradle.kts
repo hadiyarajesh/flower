3dc22aa5ec1fdfc8b527e9b5449568950cb64cc7
Piyush Johnson <piyushjohnson1998@gmail.com>
2021-03-25 19:29:30 +0530

    Update dependencies in both sample app and library

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
