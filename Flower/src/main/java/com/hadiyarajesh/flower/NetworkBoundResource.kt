800d80d96242fd4dbdb82aff1c4b18ad10f0c9aa
DatLag <46448715+DATL4G@users.noreply.github.com>
2021-03-30 15:55:28 +0200

    - added possibility to call suspend functions in networkBoundResource - provide retrofit to root project to ensure working with same version without compatibility issues - removed passing type Any to FlowCallAdapter - fixed some parsing with requirement in FlowCallAdapterFactory when using flow - provide ApiResponse for default calls (useful when calling such functions without handling in DB but relying on same service) - fixed README typos

--- Flower/src/main/java/com/hadiyarajesh/flower/NetworkBoundResource.kt
+++ Flower/src/main/java/com/hadiyarajesh/flower/NetworkBoundResource.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.flow.*
  */
 @ExperimentalCoroutinesApi
 inline fun <DB, REMOTE> networkBoundResource(
-    crossinline fetchFromLocal: () -> Flow<DB>,
-    crossinline shouldFetchFromRemote: (DB?) -> Boolean = { true },
-    crossinline fetchFromRemote: () -> Flow<ApiResponse<REMOTE>>,
-    crossinline processRemoteResponse: (response: ApiSuccessResponse<REMOTE>) -> Unit = { Unit },
-    crossinline saveRemoteData: (REMOTE) -> Unit = { Unit },
-    crossinline onFetchFailed: (errorBody: String?, statusCode: Int) -> Unit = { _: String?, _: Int -> Unit }
+    crossinline fetchFromLocal: suspend () -> Flow<DB>,
+    crossinline shouldFetchFromRemote: suspend (DB?) -> Boolean = { true },
+    crossinline fetchFromRemote: suspend () -> Flow<ApiResponse<REMOTE>>,
+    crossinline processRemoteResponse: suspend (response: ApiSuccessResponse<REMOTE>) -> Unit = { Unit },
+    crossinline saveRemoteData: suspend (REMOTE) -> Unit = { Unit },
+    crossinline onFetchFailed: suspend (errorBody: String?, statusCode: Int) -> Unit = { _: String?, _: Int -> Unit }
 ) = flow<Resource<DB>> {
 
     emit(Resource.loading(null))
@@ -43,6 +43,8 @@ inline fun <DB, REMOTE> networkBoundResource(
                         )
                     })
                 }
+
+                else -> { }
             }
         }
     } else {
---
+++ Flower/src/main/java/com/hadiyarajesh/flower/calladpater/ResultCall.kt
@@ -0,0 +1,21 @@
+package com.hadiyarajesh.flower.calladpater
+
+import com.hadiyarajesh.flower.ApiResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ResultCall<T>(proxy: Call<T>) : CallDelegate<T, ApiResponse<T>>(proxy) {
+
+    override fun enqueueImplementation(callback: Callback<ApiResponse<T>>) = proxy.enqueue(object : Callback<T> {
+        override fun onResponse(call: Call<T>, response: Response<T>) {
+            callback.onResponse(this@ResultCall, Response.success(ApiResponse.create(response)))
+        }
+
+        override fun onFailure(call: Call<T>, t: Throwable) {
+            callback.onResponse(this@ResultCall, Response.success(ApiResponse.create(t)))
+        }
+    })
+
+    override fun cloneImplementation(): Call<ApiResponse<T>> = ResultCall(proxy.clone())
+}
