75b29c8c1b3545732024128e6f49dfc4fa7572f6
Piyush Johnson <piyushjohnson1998@gmail.com>
2021-05-04 23:37:09 +0530

    Updated to support Material Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,12 +58,14 @@ dependencies {
     val retrofitVersion = "2.9.0"
     val moshiVersion = "1.11.0"
     val koinVersion = "2.1.5"
+    val materialVersion = "1.2.0"
 
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.31")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:${materialVersion}")
 
     implementation(project(":Flower"))
 
