b1265f6a6606963f673729b5a79d494f68ba2fc3
DatLag <jeff.retz08@gmail.com>
2022-09-01 21:19:42 +0200

    fixed app error

--- app/src/main/java/com/hadiyarajesh/flowersample/data/network/QuoteApi.kt
+++ app/src/main/java/com/hadiyarajesh/flowersample/data/network/QuoteApi.kt
@@ -18,11 +18,10 @@ package com.hadiyarajesh.flowersample.data.network
 
 import com.hadiyarajesh.flower_core.ApiResponse
 import com.hadiyarajesh.flowersample.data.database.entity.Quote
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface QuoteApi {
     @GET("posts/?orderby=id&per_page=1")
-    fun getRandomQuote(@Query("page") pageNo: Int): Flow<ApiResponse<List<Quote>>>
+    suspend fun getRandomQuote(@Query("page") pageNo: Int): ApiResponse<List<Quote>>
 }
