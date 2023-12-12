33af2c52d376764be2173685753e593a959d516b
hadiyarajesh <rajesh.hadiya@outlook.com>
2023-12-12 23:12:05 +0530

    removed redundant app module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,6 @@ dependencyResolutionManagement {
 rootProject.name = "flower"
 
 include(":flower-core")
-include(":app")
 include(":compose-app")
 include(":flower-retrofit")
 include("flower-ktorfit")
